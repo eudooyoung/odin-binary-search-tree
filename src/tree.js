a8b3af74abb0b15837863ccadb4404b1f85fd403
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-02-09 19:00:51 +0900

    Add preOrderForEach function

--- src/tree.js
+++ src/tree.js
@@ -1,16 +1,316 @@
-class Tree {
-  root;
-  arr;
+import Node from "./node.js";
+
+export default class Tree {
+  root = null;
 
   constructor(arr) {
     if (!arr) {
-      throw new TypeError("An array is required as an argument");
+      throw new TypeError("Argument is expected");
     }
 
-    this.arr = arr;
+    if (!Array.isArray(arr)) {
+      throw new TypeError("Argumet should be an array");
+    }
+
+    this.root = this.#buildTree(this.#filterArray(arr));
   }
 
-  buildTree(arr) {}
+  #filterArray = (arr) => {
+    const sorted = arr.toSorted((a, b) => a - b);
+    const filtered = sorted.reduce((acc, item) => {
+      if (!acc.includes(item)) {
+        acc.push(item);
+      }
+      return acc;
+    }, []);
+
+    // check filtered arr
+    // console.log(filtered);
+    return filtered;
+  };
+
+  #buildTree = (arr) => {
+    const n = arr.length;
+    if (n < 1) {
+      return null;
+    }
+
+    const mid = Math.floor(n / 2);
+    const root = new Node(arr[mid]);
+    const leftArr = arr.slice(0, mid);
+    const rightArr = arr.slice(mid + 1);
+
+    root.left = this.#buildTree(leftArr);
+    root.right = this.#buildTree(rightArr);
+
+    return root;
+  };
+
+  prettyPrint = (node = this.root, prefix = "", isLeft = true) => {
+    if (node === null || node === undefined) {
+      return;
+    }
+
+    this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  };
+
+  includes = (value) => {
+    const queue = [];
+    queue.push(this.root);
+
+    while (queue.length > 0) {
+      const current = queue.shift();
+      if (current.data === value) {
+        return true;
+      }
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+    return false;
+  };
+
+  insert = (value) => {
+    if (this.includes(value)) {
+      return;
+    }
+
+    let current = this.root;
+    while (current) {
+      if (current.data > value && current.left) {
+        current = current.left;
+      } else if (current.data < value && current.right) {
+        current = current.right;
+      } else {
+        // break the loop when
+        // current.data > value && !current.left || current.data > value && !current.right
+        break;
+      }
+    }
+
+    if (current.data > value) {
+      current.left = new Node(value);
+    } else {
+      current.right = new Node(value);
+    }
+  };
+
+  deleteItem = (value) => {
+    if (!this.includes(value)) {
+      return;
+    }
+
+    this.root = this.#delNode(this.root, value);
+  };
+
+  #delNode = (root, value) => {
+    if (!root) {
+      return null;
+    }
+
+    if (root.data > value) {
+      root.left = this.#delNode(root.left, value);
+    } else if (root.data < value) {
+      root.right = this.#delNode(root.right, value);
+    } else {
+      // Node with 0 or 1 child
+      if (!root.left) {
+        return root.right;
+      }
+      if (!root.right) {
+        return root.left;
+      }
+
+      // Node with 2 children
+      const successor = this.#getSuccessor(root);
+      root.data = successor.data;
+      root.right = this.#delNode(root.right, successor.data);
+    }
+    return root;
+  };
+
+  #getSuccessor = (current) => {
+    current = current.right;
+    while (current && current.leff) {
+      current = current.left;
+    }
+    return current;
+  };
+
+  levelOrderForEachIterative = (callback) => {
+    if (!callback || typeof callback !== "function") {
+      throw new TypeError("A callback function is required");
+    }
+
+    const queue = [];
+    queue.push(this.root);
+    while (queue.length > 0) {
+      const current = queue.shift();
+      callback(current.data);
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+  };
+
+  levelOrderForEachRecursive = (callback) => {
+    if (!callback || typeof callback !== "function") {
+      throw new TypeError("A callback function is required");
+    }
+
+    const root = this.root;
+    const levels = [];
+    this.#visitLevel(root, 0, levels);
+
+    levels.forEach((level) => {
+      level.forEach((item) => {
+        callback(item);
+      });
+    });
+  };
+
+  #visitLevel = (root, level, levels) => {
+    if (root === null) {
+      return;
+    }
+
+    if (levels.length <= level) {
+      levels.push([]);
+    }
+
+    levels[level].push(root.data);
+
+    this.#visitLevel(root.left, level + 1, levels);
+    this.#visitLevel(root.right, level + 1, levels);
+  };
+
+  inOrderForEach = (callback) => {
+    if (!callback || typeof callback !== "function") {
+      throw new TypeError("A callback function is required");
+    }
+
+    const root = this.root;
+    this.#inOrderVisit(root, callback);
+  };
+
+  #inOrderVisit = (root, callback) => {
+    if (!root) {
+      return;
+    }
+
+    if (root.left) {
+      this.#inOrderVisit(root.left, callback);
+    }
+
+    callback(root.data);
+
+    if (root.right) {
+      this.#inOrderVisit(root.right, callback);
+    }
+  };
+
+  preOrderForEach = (callback) => {
+    if (!callback || typeof callback !== "function") {
+      throw new TypeError("A callback function is required");
+    }
+
+    const root = this.root;
+    this.#preOrderVisit(root, callback);
+  };
+
+  #preOrderVisit = (root, callback) => {
+    if (!root) {
+      return;
+    }
+
+    callback(root.data);
+    if (root.left) {
+      this.#preOrderVisit(root.left, callback);
+    }
+
+    if (root.right) {
+      this.#preOrderVisit(root.right, callback);
+    }
+  };
+
+  postOrderForEach = (callback) => {
+    if (!callback || typeof callback !== "function") {
+      throw new TypeError("A callback function is required");
+    }
+
+    const root = this.root;
+    this.#postOrderVisit(root, callback);
+  };
+
+  #postOrderVisit = (root, callback) => {
+    if (!root) {
+      return;
+    }
+
+    if (root.left) {
+      this.#postOrderVisit(root.left, callback);
+    }
+
+    if (root.right) {
+      this.#postOrderVisit(root.right, callback);
+    }
+
+    callback(root.data);
+  };
+
+  height = (value) => {
+    if (!this.includes(value)) {
+      return;
+    }
+
+    const root = this.root;
+    const node = this.#findNode(root, value);
+    console.log(node);
+    // const height = this.#getHeight(node, 0);
+    // return height;
+  };
+
+  #findNode = (root, value) => {
+    const queue = [];
+    queue.push(root);
+    while (queue.length > 0) {
+      const curr = queue.shift();
+      if (curr.data === value) {
+        return curr;
+      }
+      if (curr.left) {
+        queue.push(curr.left);
+      }
+      if (curr.right) {
+        queue.push(curr.right);
+      }
+    }
+  };
+
+  // #getHeight = (root, height) => {
+  //   if (!root.left && !root.right) {
+  //     return height;
+  //   }
+
+  //   if (root.left) {
+  //     return this.#getHeight(root.left, height + 1);
+  //   }
+
+  //   if (root.right) {
+  //     return this.#getHeight(root.right, height + 1);
+  //   }
+  // };
 }
 
-export default Tree;
+// const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+// let tree = new Tree(arr);
+// tree.prettyPrint();
+// tree.height(5);
--- src/tree.test.js
+++ src/tree.test.js
@@ -1,30 +1,98 @@
 import Tree from "./tree.js";
 
-describe("tree class test", () => {
+describe("Tree test", () => {
+  let tree;
+  const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+  beforeEach(() => {
+    tree = new Tree(arr);
+  });
+
   it("class defined", () => {
     expect(Tree).toBeDefined();
   });
 
   it("constructor requirement", () => {
-    expect(() => new Tree()).toThrow(TypeError);
-
-    let arr = [];
-    expect(() => new Tree(arr)).not.toThrow(TypeError);
+    expect(() => new Tree("abc")).toThrow(TypeError);
   });
 
-  it("instance properties", () => {
-    let arr = [];
-    const tree = new Tree(arr);
-
-    expect(tree).toHaveProperty("root");
-    expect(tree).toHaveProperty("arr");
+  it("root", () => {
+    expect(tree.root.data).toBe(8);
   });
 
-  it("buildTree function", () => {
-    const tree = new Tree([]);
-    const buildTree = tree.buildTree;
+  it("includes function", () => {
+    expect(tree.includes(1)).toBe(true);
+    expect(tree.includes(2)).toBe(false);
+    expect(tree.includes(3)).toBe(true);
+    expect(tree.includes(6)).toBe(false);
+  });
 
-    expect(buildTree).toBeDefined();
-    expect(typeof buildTree).toBe("function");
+  it("insert function", () => {
+    tree.insert(6);
+    expect(tree.includes(6)).toBe(true);
+    tree.insert(10);
+    expect(tree.includes(10)).toBe(true);
+  });
+
+  it("deleteItem function", () => {
+    tree.deleteItem(1);
+    expect(tree.includes(1)).toBe(false);
+    tree.deleteItem(7);
+    expect(tree.includes(7)).toBe(false);
+    tree.deleteItem(-1);
+    expect(tree.includes(-1)).toBe(false);
+  });
+
+  it("levelOrderForEach function", () => {
+    let arr = [];
+    const callback = (data) => arr.push(data);
+    tree.levelOrderForEachIterative(callback);
+    expect(arr).toEqual([8, 4, 67, 3, 7, 23, 6345, 1, 5, 9, 324]);
+
+    expect(() => tree.levelOrderForEachIterataive()).toThrow(TypeError);
+  });
+
+  it("levelOrderForEachRecurive function", () => {
+    let arr = [];
+    const callback = (data) => arr.push(data);
+    tree.levelOrderForEachRecursive(callback);
+    expect(arr).toEqual([8, 4, 67, 3, 7, 23, 6345, 1, 5, 9, 324]);
+
+    expect(() => tree.levelOrderForEachRecursive()).toThrow(TypeError);
+  });
+
+  it("inOrderForEach function", () => {
+    let arr = [];
+    const callback = (data) => arr.push(data);
+    tree.inOrderForEach(callback);
+    expect(arr).toEqual([1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]);
+
+    expect(() => tree.inOrderForEach()).toThrow(TypeError);
+  });
+
+  it("preOrderForEach function", () => {
+    let arr = [];
+    const callback = (data) => arr.push(data);
+    tree.preOrderForEach(callback);
+    expect(arr).toEqual([8, 4, 3, 1, 7, 5, 67, 23, 9, 6345, 324]);
+
+    expect(() => tree.preOrderForEach()).toThrow(TypeError);
+  });
+
+  it("postOrderForEach function", () => {
+    let arr = [];
+    const callback = (data) => arr.push(data);
+    tree.postOrderForEach(callback);
+    expect(arr).toEqual([1, 3, 5, 7, 4, 9, 23, 324, 6345, 67, 8]);
+
+    expect(() => tree.preOrderForEach()).toThrow(TypeError);
+  });
+
+  it("height function", () => {
+    // expect(tree.height(-1)).toBeUndefined();
+    // expect(tree.height(1)).toBe(0);
+    // expect(tree.height(3)).toBe(1);
+    // expect(tree.height(4)).toBe(2);
+    expect(tree.height(5)).toBe(0);
   });
 });
